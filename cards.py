21349b98f6f461e4460c9ba85e6de6770f38c8a4
kvo18u333 <krivozubovvo@student.bmstu.ru>
2019-05-05 23:31:08 +0300

    ADD: 3 new card classes

--- cards.py
+++ cards.py
@@ -32,8 +32,48 @@ class Init_declaration_card:
     def assign_name(self, char_variable):
         char_variable.set_name(self.name)
 
-    def assign_value():
+    def assign_value(self, char_variable):
         char_variable.set_value(self.value)
 
     self.name = None
     self.value = None
+
+# Карточка для присваивания переменных типа char(char a, char b: a = b)
+class Value_to_value_card:
+    # left_char, right char - объекты класа char
+    def __init__(self, left_char, right_char):
+        self.left = left_char
+        self.right = right_char
+
+    def assign():
+        self.left.set_value(self.right.get_value())
+    
+    self.left = None
+    self.right = None
+
+# Карточка объявления указателя (char *a;)
+class Init_pointer_card:
+    def __init__(self, pointer_name):
+        self.name = pointer_name
+    
+    def assign_name(self, pointer_variable):
+        pointer_variable.set_name(self.name)
+
+    self.name = None
+
+# Карточка инициализации указателя (a = &b;)
+#class Declaration_pointer_card:
+ 
+
+# Карточка putchar(char a);
+class Putchar_card:
+    def __init__(self, char_variable):
+        self.char_object = char_variable
+    def output():
+        # здесь идет связь с GUI(метод output возвратит вам значение переменной типа char) 
+        return self.char_object.get_value()
+
+    self.char_object = None
+
+#class Putchar_pointer_card:
+    
