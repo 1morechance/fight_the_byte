3301c28237f2c287460ba208ceb7a518e340b782
kvo18u333 <krivozubovvo@student.bmstu.ru>
2019-05-06 22:50:35 +0300

    start realizing new construct with Init_card class

--- cards.py
+++ cards.py
@@ -1,24 +1,41 @@
+char_case = []
+pointer_char_case = []
+# Массивы, хранящие все созданные переменные(объекты классов Char и )
+
 import char
 
 # char_variable - объект класса char, определеленный в ветке char
 
 # Карточка инициализации существующей переменной (a = 'L';)
 class Init_card:
-    def __init__(self, char_value):
-        self.value = card_value
+    def __init__(self, char_name, char_value):
+        self.value = char_value
+        self.name = char_name
 
-    def assign_value(self, char_variable):
-        char_variable.set_value(self.value)
-    
-    self.value = None
+    def action(self):
+        global char_case
+        is_exist = False
+        for var in char_case:
+            print(var.get_name())
+            if var.get_name() == self.name:
+                char_variable.set_value(self.value)
+                is_exist = True
+                break
+        if (not is_exist):
+            raise RuntimeError("Ошибка компиляции: " + self.name + " не была объявлена ранее")
+
+    value = None
+    name = None
 
 # Карточка для объявления переменной (char a;)
 class Declaration_card:
     def __init__(self, char_name):
-        self.name = card_name
+        self.name = char_name
 
     def assign_name(self, char_variable):
         char_variable.set_name(self.value)
+
+    
 
     self.name = None
 
@@ -63,13 +80,13 @@ class Init_pointer_card:
 
 # Карточка инициализации указателя (a = &b;)
 #class Declaration_pointer_card:
- 
+
 
 # Карточка putchar(char a);
 class Putchar_card:
     def __init__(self, char_variable):
         self.char_object = char_variable
-    def output():
+    def output(self):
         # здесь идет связь с GUI(метод output возвратит вам значение переменной типа char) 
         return self.char_object.get_value()
 
