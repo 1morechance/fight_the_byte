4867e1c0e7a8e36244471629182e4e514a1a4062
Dmitry Blokhin <695-bdm18u148@users.noreply.git.iu7.bmstu.ru>
2019-05-14 22:52:09 +0300

    Upload game_process_menu.py

---
+++ game_process_menu.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Desktop/Work/GUI/game_process_menu.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1440, 785)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 1440, 900))
+        self.label.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("Game_process_png.png"))
+        self.label.setObjectName("label")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(50, 210, 421, 611))
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.frame_2 = QtWidgets.QFrame(self.centralwidget)
+        self.frame_2.setGeometry(QtCore.QRect(550, 260, 351, 551))
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.frame_3 = QtWidgets.QFrame(self.centralwidget)
+        self.frame_3.setGeometry(QtCore.QRect(980, 210, 421, 611))
+        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_3.setObjectName("frame_3")
+        self.Next_turn_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Next_turn_button.setGeometry(QtCore.QRect(580, 40, 281, 81))
+        self.Next_turn_button.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.Next_turn_button.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("Next_turn_button_png.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.Next_turn_button.setIcon(icon)
+        self.Next_turn_button.setIconSize(QtCore.QSize(571, 111))
+        self.Next_turn_button.setFlat(False)
+        self.Next_turn_button.setObjectName("Next_turn_button")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+
