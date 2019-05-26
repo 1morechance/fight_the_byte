941eaafef804802c2ec1e4ad0c3f3c7685022982
kvo18u333 <krivozubovvo@student.bmstu.ru>
2019-05-27 00:16:18 +0300

    CHANGE: main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from cards import*
 from randomizer import generate_draft
 from pandas import DataFrame
+from check_win import check_winner
+
+# Учёт переменных общий и ведется в файле cards
+
+# Стеки с кодом каждого игрока
+stack_player_1 = []
+stack_player_2 = []
 
 HAND_SIZE = 8
 WIN_BY = 'лещ'
@@ -8,10 +15,22 @@ WIN_BY = 'лещ'
 data = DataFrame()
 hand = (generate_draft(HAND_SIZE, data, WIN_BY))[1]
 
-for card in hand:
-    print(card.view())
+# Когда ход завершен, выполняется интерпретация
+# По одной строчке
+# Начиная с игрока который начинал игру
+
+win = 0
+LEFT_PLAYER = 1
+RIGHT_PLAYER = 2
+
+# В переменную current_out подается текст из поля вывода
 try:
-    for card in hand:
-        card.action()
+    for num in range(len(stack_player_1)):
+        stack_player_1[num].action()
+        if (check_win(current_output, WIN_BY)):
+            win = LEFT_PLAYER
+        stack_player_2[num].action()
+        if (check_win(current_output, WIN_BY)):
+            win = RIGHT_PLAYER
 except RuntimeError as err:
     print(err)
