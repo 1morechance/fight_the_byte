1274bfedf9a51e76703795a3dcd9f1c53ab33d40
Sarkisov Artem <635-sas18u476@users.noreply.git.iu7.bmstu.ru>
2019-05-27 19:27:35 +0300

    ADD: Final one.

--- main.py
+++ main.py
@@ -69,7 +69,13 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
 
         self.First_player_stack = []
         self.Second_player_stack = []
-        self.word = "fook"
+        
+        self.word = "FOOK"
+
+        self.Current_word.setText(self.word)
+        self.First_player_nickname.setText('CONOR')
+        self.Second_player_nickname.setText('KHABIB')
+        
         self.winner = 0
 
         self.data = pd.DataFrame()  # Информация о переменных (для невменяемых)
@@ -98,7 +104,6 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
 
     def Next_turn(self):
         self.Turn += 1
-        print(self.Turn)
         if (self.Turn % 2 == 0):
             clean()
             self.winner = interpretation(self.First_player_stack, self.Second_player_stack, self.word)
@@ -107,6 +112,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
         
     def Card_0_add(self):
         self.Next_turn()
+        self.Output_label.setText("0")
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[0] + "\n"
             self.First_player_stack.append(self.data_card[0])
@@ -119,6 +125,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card0.setText('')
 
     def Card_1_add(self):
+        self.Output_label.setText("1")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[1] + "\n"
@@ -132,6 +139,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card1.setText('')
 
     def Card_2_add(self):
+        self.Output_label.setText("2")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[2] + "\n"
@@ -145,6 +153,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card2.setText('')
 
     def Card_3_add(self):
+        self.Output_label.setText("3")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[3] + "\n"
@@ -158,6 +167,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card3.setText('')
 
     def Card_4_add(self):
+        self.Output_label.setText("4")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[4] + "\n"
@@ -171,6 +181,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card4.setText('')
 
     def Card_5_add(self):
+        self.Output_label.setText("5")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[5] + "\n"
@@ -184,6 +195,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card5.setText('')
 
     def Card_6_add(self):
+        self.Output_label.setText("6")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[6] + "\n"
@@ -197,6 +209,7 @@ class Play_Window(QtWidgets.QMainWindow, game_process_menu.Ui_MainWindow):
             self.Card6.setText('')
 
     def Card_7_add(self):
+        self.Output_label.setText("7")
         self.Next_turn()
         if (self.Turn % 2 == 0):
             self.First_player_label_text += self.data_card_text[7] + "\n"
