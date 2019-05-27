d4ad34f2d11b6fbf92d13f7099492d01084a1c6f
Maxim <maxztrololo@gmail.com>
2019-05-27 20:43:20 +0300

    doxygen added

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['search',['search',['../classcards_1_1_value__to__value__p.html#a4913693a20f937a175a8668979a45700',1,'cards::Value_to_value_p']]]
+  ['retranslateui',['retranslateUi',['../classmain__menu_1_1_ui___main_window.html#afc5b3b59947cb086923048a8fda86243',1,'main_menu::Ui_MainWindow']]]
 ];
---
+++ html/search/namespaces_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['randomizer',['randomizer',['../namespacerandomizer.html',1,'']]],
+  ['randomizer1',['randomizer1',['../namespacerandomizer1.html',1,'']]]
+];
